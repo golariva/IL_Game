f3acc3f7307a7460d50f3c004d563d3d65bd185c
golariva <aagolubeva@edu.hse.ru>
2023-02-15 09:13:29 +0500

    добавлена вторая мини-игра

---
+++ Assets/University/Pairs/AddButtons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Transform puzzleField;
+
+    [SerializeField]
+    private GameObject btn;
+
+    //первая функция, которая заработает при запуске скрипта
+    void Awake()
+    {
+        for (int i = 0; i < 36; i++)
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
+    }
+}
